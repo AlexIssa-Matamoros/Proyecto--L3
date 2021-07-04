3609f0daf0768780d714dda4d4676f4df3f3ed69
Issa Matt <Issa Matt@DESKTOP-0SA65VC>
2021-07-04 15:35:16 -0600

    Agregando Validacion de eliminar

--- Supermercado/Win.Supermercado/FrmProductos.Designer.cs
+++ Supermercado/Win.Supermercado/FrmProductos.Designer.cs
@@ -166,6 +166,7 @@
             this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
             this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
             this.bindingNavigatorDeleteItem.Text = "Eliminar";
+            this.bindingNavigatorDeleteItem.Click += new System.EventHandler(this.bindingNavigatorDeleteItem_Click);
             // 
             // bindingNavigatorMoveFirstItem
             // 
@@ -267,6 +268,7 @@
             this.idTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.listaProductosBindingSource, "Id", true));
             this.idTextBox.Location = new System.Drawing.Point(129, 39);
             this.idTextBox.Name = "idTextBox";
+            this.idTextBox.ReadOnly = true;
             this.idTextBox.Size = new System.Drawing.Size(262, 20);
             this.idTextBox.TabIndex = 8;
             // 
@@ -286,6 +288,7 @@
             this.toolStripButtonCancelar.Name = "toolStripButtonCancelar";
             this.toolStripButtonCancelar.Size = new System.Drawing.Size(57, 22);
             this.toolStripButtonCancelar.Text = "Cancelar";
+            this.toolStripButtonCancelar.Click += new System.EventHandler(this.toolStripButtonCancelar_Click);
             // 
             // listaProductosBindingSource
             // 
--- Supermercado/Win.Supermercado/FrmProductos.cs
+++ Supermercado/Win.Supermercado/FrmProductos.cs
@@ -40,13 +40,14 @@ namespace Win.Supermercado
 
             var resultado = _productos.GuardarProducto(producto);
 
-            if (resultado == true)
+            if (resultado.Exitoso == true)
             {
                 listaProductosBindingSource.ResetBindings(false);
+                DesabilitarHabilitarBotones(true);
             }
             else
             {
-                MessageBox.Show("OCURRIO UN ERROR AL MOMENTO DE GUARDAR");
+                MessageBox.Show(resultado.Mensaje);
             }
         }
 
@@ -54,6 +55,54 @@ namespace Win.Supermercado
         {
             _productos.AgregarProducto();
             listaProductosBindingSource.MoveLast();
+
+            DesabilitarHabilitarBotones(false);
+        }
+
+        private void DesabilitarHabilitarBotones(bool valor)
+        {
+            bindingNavigatorMoveFirstItem.Enabled = valor;
+            bindingNavigatorMoveLastItem.Enabled = valor;
+            bindingNavigatorMovePreviousItem.Enabled = valor;
+            bindingNavigatorMoveNextItem.Enabled = valor;
+
+            bindingNavigatorAddNewItem.Enabled = valor;
+            bindingNavigatorDeleteItem.Enabled = valor;
+            toolStripButtonCancelar.Visible = !valor;
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            if (idTextBox.Text != "")
+            {
+                var resultado = MessageBox.Show("Desea eliminar este registro?", "Eliminar", MessageBoxButtons.YesNo);
+                if (resultado == DialogResult.Yes)
+                {
+                    var id = Convert.ToInt32(idTextBox.Text);
+                    Eliminar(id);
+                }
+           
+            }
+        }
+
+        private void Eliminar(int id)
+        {
+            var resultado = _productos.EliminarProducto(id);
+
+            if (resultado == true)
+            {
+                listaProductosBindingSource.ResetBindings(false);
+            }
+            else
+            {
+                MessageBox.Show("OCURRIO UN ERROR AL QUERER ELIMINAR EL PRODUCTO");
+            }
+        }
+
+        private void toolStripButtonCancelar_Click(object sender, EventArgs e)
+        {
+            DesabilitarHabilitarBotones(true);
+            Eliminar(0);
         }
     }
 }
